4a1dcbb2ce54dbe37f89e53bd50e15488e6210dc
icomanman <fred_barabs@yahoo.com>
2021-03-28 15:57:39 +0800

    updated vectorSetup for p1-3

--- 204/SOR.m
+++ 204/SOR.m
@@ -1,8 +1,13 @@
 
-function x = SOR(A, h, n, tol)
-
-  for i = 1 : n
-    
+function x = SOR(A)
+	tol = 10^(-4);
+	n = 30;
+	h = (1 / (1 + n));
+  L = tril(A);
+  U = triu(A);
+  D = diag(A);
+  
+  for i = 1 : 30
   end
 
 end
--- 204/vectorSetup.m
+++ 204/vectorSetup.m
@@ -1,49 +1,21 @@
-function [mod_A, h, x] = vectorSetup(A, n, init_x)
+% 28 Mar 2021
+% A is the problem's tri-diagonal matrix
+% b is the problem's given vector
+% n is the size of matrix/vector length
+function [A, b] = vectorSetup(n)
 
   % indices column vector
   idx = [1:n]';
   % initialize h values
-  %h = (1 ./ (idx + 1)) .^2;
-  h = (1/(n + 1))^2;
-
-  % creating the modified matrix A after isolating x values on the left side
-  mod_A = zeros(n);
-  x_coeff = diag(A);
-  x_init = ones(n, 1);
+  h_el = (1/(n + 1))^2;
   
-  mod_A(1,:) = [h, -1 * x_init(2), zeros(1,4)] ./ x_coeff(1);
-  mod_A(n,:) = [zeros(1,4), -1 * x_init(n-1), h] ./ x_coeff(n);
+  % creating vector b:
+  b = ones(n, 1) * h_el;
   
-  for i = 2 : (n-1)
-  
-    mod_A(i,i) = h / x_coeff(i);
-    mod_A(i,i-1) = -1 * x_init(i - 1) / x_coeff(i);
-    mod_A(i,i+1) = -1 *x_init(i + 1) / x_coeff(i);
-
+  % creating the tri-diagonal matrix A
+  A = ones(n);
+  LA = tril(A, -1) - tril(A, -2);
+  UA = triu(A, 1) - triu(A, 2);
+  A = A .* diag(diag(A) * -2);
+  A = A + LA + UA;
   end
-  
-  for j = 1:50
-    init_x = mod_A * (h * ones(n,1));
-    mod_A(1,:) = [h, -1 * init_x(2), zeros(1,4)] ./ x_coeff(1);
-    mod_A(n,:) = [zeros(1,4), -1 * init_x(n-1), h] ./ x_coeff(n);
-  
-    for i = 2 : (n-1)
-  
-      mod_A(i,i) = h / x_coeff(i);
-      mod_A(i,i-1) =  -1 * init_x(i - 1) / x_coeff(i);
-      mod_A(i,i+1) =  -1 * init_x(i + 1) / x_coeff(i);
-
-    end
-    
-    init_x = mod_A * (h * ones(n,1));
-    
-    plot(j, init_x(1), "*r");
-    plot(j, mod_A(1,2), "og");
-    hold on
-    
-  end
-  hold off
-  x = init_x;
-  
-  
-end
