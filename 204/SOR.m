c5987a3f8f46da9af5c4cc91d9d4e7d0238523dc
icomanman <fred_barabs@yahoo.com>
2021-03-28 23:01:32 +0800

    SOR diverging

--- 204/SOR.m
+++ 204/SOR.m
@@ -5,7 +5,7 @@ function x = SOR(A, b)
   omegs = [1.0: 0.1: 1.9];
   
   % init x:
-  x = ones(length(b), 1);
+  x_init = ones(length(b), 1);
   
   LA = tril(A, -1);
   UA = triu(A, 1);
@@ -23,28 +23,33 @@ function x = SOR(A, b)
   if invertible == true
     diff = 1;
     limit = 1; % just in case...
-    om = 1.1;
+    om = 1;
     
-    while diff > tol && limit <= 500 || x(1) > 1e6
-     [~, ~, LInf] = vectorNorm(x'); % vectorNorm only accepts a row vector
+    while diff > tol && limit <= 500
+     [~, ~, LInf] = vectorNorm(x_init'); % vectorNorm only accepts a row vector
      LInf0 = LInf;
-     %div = (D + LA)^1; 
-     %x = div * (b - ((UA + (0 * D)) * x));
-     
+
      sum_DL_inv = (D - (om * LA))^1;
-     D_om =(1 - om) * D;
+     D_om = (1 - om) * D;
      U_om = om * UA;
      
-     x = (sum_DL_inv * (U_om + D_om) * x) + ((om * sum_DL_inv) * b);
+     x = (sum_DL_inv * (U_om + D_om) * x_init) + ((om * sum_DL_inv) * b);
+     x_init = x;
      
      fprintf("After %i iterations: %d\n", limit, x(1));
           
-     [~, ~, LInf] = vectorNorm(x'); % vectorNorm only accepts a row vector
+     [~, ~, LInf] = vectorNorm(x_init'); % vectorNorm only accepts a row vector
      diff = abs(LInf0 - LInf);
      limit = limit + 1;
      
-     plot(limit, x(1), "go");
+     plot(limit, x(1), 'b+', "LineWidth", 2);
      hold on
+     
+     if diff > 50
+       fprintf("break: %d", diff);
+      break
+     end
+     
     end
     
     hold off
