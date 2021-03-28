191f91b5d824f29867790698de09c45156ec8618
icomanman <fred_barabs@yahoo.com>
2021-03-28 18:58:10 +0800

    Solving SOR

--- 204/SOR.m
+++ 204/SOR.m
@@ -1,13 +1,47 @@
 
-function x = SOR(A)
+function x = SOR(A, b)
+% tol and omegs are harcoded for this problem
 	tol = 10^(-4);
-	n = 30;
-	h = (1 / (1 + n));
-  L = tril(A);
-  U = triu(A);
-  D = diag(A);
+  omegs = [1.0: 0.1: 1.9];
   
-  for i = 1 : 30
+  % init x:
+  x = ones(length(b), 1);
+  
+  LA = tril(A, -1);
+  UA = triu(A, 1);
+  D = diag(diag(A));
+  invertible = false;
+  
+  try
+    if det(A) ~= 0
+      invertible = true;
+    end
+  catch
+    fprintf("> Err: Matrix A is singular.");
   end
-
+    
+  if invertible == true
+    diff = 1;
+    limit = 1; % just in case...
+    
+    
+    while diff > tol && limit <= 500
+     [~, ~, LInf] = vectorNorm(x'); % vectorNorm only accepts a row vector
+     LInf0 = LInf;
+     div = (D + LA)^1 ; 
+     x = div * (b - ((UA + (0 * D)) * x));
+     fprintf("After %i iterations: %d\n", limit, x(1));
+          
+     [~, ~, LInf] = vectorNorm(x'); % vectorNorm only accepts a row vector
+     diff = abs(LInf0 - LInf);
+     limit = limit + 1;
+     
+     plot(limit, x(1), "go");
+     hold on
+    end
+    
+    hold off
+    
+  end
+  
 end
--- 204/vectorNorm.m
+++ 204/vectorNorm.m
@@ -2,20 +2,20 @@ function [L1, L2, LInf] = vectorNorm(v)
 
 % Check if there's an actual input
 if ~exist('v', "var")
-  fprintf("> ERROR: Please input a proper vector.\n");
+  fprintf("> ERROR: Please input a proper vector (null).\n");
   return;
 end
 
 % Check if it's empty or a scalar
 if length(v) < 2 || isempty(v)
-  fprintf("> ERROR: Please input a proper vector.\n");
+  fprintf("> ERROR: Please input a proper vector (scalar/empty).\n");
   %L1 = NaN; L2 = NaN; LInf = NaN;
   return;
 else
   % otherwise, if it is a matrix
   [row, ~] = size(v);
   if row > 1
-    fprintf("> ERROR: Please input a proper vector.\n");
+    fprintf("> ERROR: Please input a proper vector (matrix).\n");
     return;
   end
 end
