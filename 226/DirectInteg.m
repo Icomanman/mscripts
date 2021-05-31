da4e960674a0f28012c8f1c30654eff59c0ee0e6
icomanman <fred_barabs@yahoo.com>
2021-05-31 16:27:35 +0800

    updated DirectInteg.

--- 226/DirectInteg.m
+++ 226/DirectInteg.m
@@ -7,6 +7,7 @@ function u = DirectInteg(acc, t)
 
   u(1) = 0;
   u_dot(1) = 0;
+  
   for i = 1 : n - 1
     u_dot(i + 1) = u_dot(i) + (g * 0.5 * (acc(i) + acc(i + 1)) * step);
     u(i + 1) = u(i) + (0.5 * (u_dot(i) + u_dot(i + 1)) * step); 
@@ -14,10 +15,13 @@ function u = DirectInteg(acc, t)
 
   u_dot_max = max(u_dot);
   u_dot_min = min(u_dot);
-  t_idx_min = find(u_dot == u_dot_min);
-  t_idx_max = find(u_dot == u_dot_max);
-  fprintf("Min: %d at %d. \n", u_dot_min, t(t_idx_min));
-  fprintf("Max: %d at %d. \n", u_dot_max, t(t_idx_max));
-  % plot(t, u_dot);
+  
+  fprintf("Min: %f at %f. \n", u_dot_min, t(u_dot == u_dot_min));
+  fprintf("Max: %f at %f. \n", u_dot_max, t(u_dot == u_dot_max));
+  
+  plot(t, u_dot, "LineWidth", 1.5);
+  grid on;
+  xlim([0 20]);
+  ylim([-30 30]);
 
 end
