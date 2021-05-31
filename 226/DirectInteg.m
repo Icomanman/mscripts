6a79d801829734988ed2ac9fdf1e0f1be7597586
Icomanman <fred_barabs@yahoo.com>
2021-05-31 10:34:53 +0800

    updated DirectInteg; values match with cumtrapz.

--- 226/DirectInteg.m
+++ 226/DirectInteg.m
@@ -1,13 +1,15 @@
 function u = DirectInteg(acc, t)
   g = 386.08858;
   n = length(acc);
+  u = zeros(n, 1);
   u_dot = zeros(n, 1);
-  % u = zeros(n, 1);
   step = t(2) - t(1);
 
+  u(1) = 0;
   u_dot(1) = 0;
   for i = 1 : n - 1
-    u_dot(i + 1) = u_dot(i) + (g * acc(i) * step); 
+    u_dot(i + 1) = u_dot(i) + (g * 0.5 * (acc(i) + acc(i + 1)) * step);
+    u(i + 1) = u(i) + (0.5 * (u_dot(i) + u_dot(i + 1)) * step); 
   end  
 
   u_dot_max = max(u_dot);
