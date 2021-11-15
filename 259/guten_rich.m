a316e149976672e381b6ca8c3e34d7865ffc6347
icomanman <fred_barabs@yahoo.com>
2021-11-15 16:22:29 +0800

    added guten rich.

--- 259/guten_rich.m
+++ 259/guten_rich.m
@@ -23,11 +23,12 @@ Eq(6) = 0.7 + R;
 % Total number divided by the period considered:
 lambda = Eq ./ 400;
 
-% vector of magnitudes from 2.5 to 7.5:
-m = linspace(2.5, 7.5, 6)';
+% vector of magnitudes from 2 to 7.5:
+m = linspace(3, 8, 6)';
 
 % plot lambda vs m:
 semilogy(m, lambda, "*", "Linewidth", 2);
+% plot(m, log10(lambda), "*", "Linewidth", 2);
 hold on;
 grid on;
 xlabel("Magnitude, m");
@@ -38,10 +39,19 @@ ylabel("Annual Rate of Exceedance, \bf \lambda_{m}");
 M = ones(6, 2);
 M(:,2) = m;
 
-B = (lambda \ M);
+B = (M \ log10(lambda));
 a = B(1);
 b = B(2);
+
+ax = 4.92677;
+bx = -0.98563;
+
 fprintf("a = %f, b = %f\n", a, b);
 % plot the regression line:
-fplot(@(m) 10^(a - b.*m), [2.5, 7.5], "Linewidth", 2);
+y = ones(6, 1);
+for i = 1 : 6
+  y(i) = (a + (b * (m(i))));
+end
+semilogy(m, 10.^y, "Linewidth", 2);
+% fplot(@(s) 10^(a + (b * (s))), [3, 8], "Linewidth", 2);
 hold off;
